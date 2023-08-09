7b6bedeee57acbb3d552fecd4b8b8106ba77f75a
DanielNehemiah <microdanysoft@gmail.com>
2023-08-09 21:41:09 +0530

    first commit

---
+++ code_trials.py
@@ -0,0 +1,26 @@
+# 9th August. 2023
+
+print("We are programmers!")
+
+print("56"+"56")
+
+print("DAN"+"NY")
+
+print(40+56)
+
+name = "Prince"
+print(name+"Z")
+print("name"+"Z")
+
+print(name*2)
+
+name = "Prince "
+print(name*2)
+
+name = "Prince"
+print(name, name)
+
+print(name+" "+name)
+
+sp = "X"
+print(name+sp+name)
---
+++ notes.md
@@ -0,0 +1,11 @@
+# Notes
+## 9th August. 2023
+
+1. Data
+2. Data type
+3. Variable
+4. Data Structure
+5. SYNTAX: string, array, function call syntax
+6. Running a small code
+7. Integer operations
+8. String operations
